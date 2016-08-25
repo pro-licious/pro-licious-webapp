6f951615da9f229a3a7d930b1bd66fd040005480
Prasanna Kapate <prasanna.1616@gmail.com>
2016-08-26 00:27:50 +0530

    initial setup

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -3,8 +3,8 @@
 import angular from 'angular';
 import angularUIRouter from 'angular-ui-router';
 import uiBootstrap from 'angular-ui-bootstrap';
-import LoginCtrl from './login/LoginCtrl';
-import DashboardCtrl from './dashboard/dashboard-ctrl';
+import LoginCtrl from './login/login-controller';
+import DashboardCtrl from './dashboard/dashboard-controller';
 import '../styles/style.scss';
 import './login/login.scss';
 import './dashboard/dashboard.scss';
@@ -12,19 +12,22 @@ import './login/login.scss';
 
 var app = angular.module('app', [angularUIRouter, uiBootstrap]);
 
-app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-	$stateProvider
-		.state('login', {
-			url: '/login',
-			templateUrl: './app/login/login.html',
-			controller: LoginCtrl,
-			controllerAs: 'vm'
-		})
-		.state('dashboard', {
-			url: '/dashboard',
-			templateUrl: './app/dashboard/dashboard.html',
-			controller: DashboardCtrl,
-			controllerAs: 'vm'
-		});
-	$urlRouterProvider.otherwise('/login');
-}]);
+app.config(['$stateProvider', '$urlRouterProvider',
+	function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('login', {
+				url: '/login',
+				templateUrl: './app/login/login.html',
+				controller: LoginCtrl,
+				controllerAs: 'vm'
+			})
+			.state('dashboard', {
+				url: '/dashboard',
+				templateUrl: './app/dashboard/dashboard.html',
+				controller: DashboardCtrl,
+				controllerAs: 'vm'
+			});
+
+		$urlRouterProvider.otherwise('/login');
+	}
+]);
