b1e7d6bbd52257d379ce41a1b4d66a835f8727e9
Prasanna Kapate <prasanna.1616@gmail.com>
2017-04-11 10:34:45 +0530

    heroku file changes

---
+++ server.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+(function() {
+	'use strict';
+	var port = process.env.PORT || 5000;
+	var environment = process.env.NODE_ENV;
+	var express = require('express'),
+		app = express();
+
+	app.use(express.static('www'));
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+	app.all('*', function(req, res, next) {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+		next();
+	});
+
+// API Routes
+
+	app.set('port', port);
+
+	app.listen(app.get('port'), function() {
+		console.log('Express server listening on port ' + app.get('port'));
+	});
+})();
